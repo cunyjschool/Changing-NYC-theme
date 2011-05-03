93d66798611b92671ebe72ebeefeb23ce1fd9687
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-05-03 18:07:34 +0000

    Other base files

---
+++ footer.php
@@ -0,0 +1,14 @@
+<div class="footer">
+	
+	<div class="wrap">
+		
+	</div><!-- END .wrap -->
+	
+	<div class="clear"></div>
+	
+</div><!-- END .footer -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
