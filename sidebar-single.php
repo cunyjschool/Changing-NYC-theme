1f26153181d327e06a0fb68ca03c87b02754e871
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-05-09 17:54:02 +0000

    Move the share buttons to the left

---
+++ sidebar-single.php
@@ -0,0 +1,7 @@
+<div class="sidebar float-right">
+	
+	<div class="share-options sidebar-item">
+		<span class="facebook-share"><iframe src="http://www.facebook.com/plugins/like.php?href&amp;send=true&amp;layout=button_count&amp;width=100&amp;show_faces=false&amp;action=recommend&amp;colorscheme=light&amp;font&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:21px;" allowTransparency="true"></iframe></span><span class="share-twitter"><a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal" data-via="cunyjschool">Tweet</a></span>
+	</div>
+		
+</div>
