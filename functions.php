b2d1d51e7f66feaffe3197c5890190a47ff6b180
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-05-23 21:40:58 +0000

    Style the comments

--- functions.php
+++ functions.php
@@ -510,28 +510,40 @@ function twentyten_comment( $comment, $args, $depth ) {
 		case '' :
 	?>
 	<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
+		
 		<div id="comment-<?php comment_ID(); ?>">
+			
+		<div class="comment-meta float-right">			
 		<div class="comment-author vcard">
-			<?php echo get_avatar( $comment, 40 ); ?>
-			<?php printf( __( '%s <span class="says">says:</span>', 'twentyten' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
+			<div class="avatar float-right">
+				<?php echo get_avatar( $comment, 48 ); ?>
+			</div>
+			<?php comment_author_link(); ?>
 		</div><!-- .comment-author .vcard -->
-		<?php if ( $comment->comment_approved == '0' ) : ?>
-			<em><?php _e( 'Your comment is awaiting moderation.', 'twentyten' ); ?></em>
-			<br />
-		<?php endif; ?>
 
-		<div class="comment-meta commentmetadata"><a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>">
+		<div class="commentmetadata"><a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>">
 			<?php
 				/* translators: 1: date, 2: time */
 				printf( __( '%1$s at %2$s', 'twentyten' ), get_comment_date(),  get_comment_time() ); ?></a><?php edit_comment_link( __( '(Edit)', 'twentyten' ), ' ' );
 			?>
-		</div><!-- .comment-meta .commentmetadata -->
+		</div><!-- END .commentmetadata -->
+		
+		</div><!-- END .comment-meta -->
 
-		<div class="comment-body"><?php comment_text(); ?></div>
+		<div class="comment-body">
+			<?php comment_text(); ?>
+		</div>
+		
+		<?php if ( $comment->comment_approved == '0' ) : ?>
+			<p class="awaiting-moderation"><em><?php _e( 'Your comment is awaiting moderation.', 'twentyten' ); ?></em></p>
+		<?php endif; ?>		
 
 		<div class="reply">
 			<?php comment_reply_link( array_merge( $args, array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
 		</div><!-- .reply -->
+		
+		<div class="clear-right"></div>
+		
 	</div><!-- #comment-##  -->
 
 	<?php
