381d21199bf509f70fce1ef363872079ce1eab59
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-05-24 16:17:20 +0000

    Show names of contributors in the footer

--- footer.php
+++ footer.php
@@ -4,7 +4,23 @@
 	
 	<div class="wrap">
 		
-		<p>A project of the NY City News Service</p>
+		<?php
+			$args = array(
+				'orderby' => 'display_name',
+				'role' => 'author',
+			);
+			$all_authors = get_users( $args );
+			$authors_html = '';
+			foreach ( $all_authors as $key => $author ) {
+				$authors_html .= '<a href="' . get_author_posts_url( $author->ID) . '">' . $author->display_name . '</a>, ';
+				if ( $key == ( count( $all_authors ) - 2 ) ) {
+					$authors_html .= 'and ';
+				}
+			}
+			$authors_html = rtrim( $authors_html, ', ' ); 
+		?>
+		
+		<p>A project of the NY City News Service<?php if ( $authors_html ) : ?> with contributions from <?php echo $authors_html; ?><?php endif; ?></p>
 		
 	</div><!-- END .wrap -->
 	
